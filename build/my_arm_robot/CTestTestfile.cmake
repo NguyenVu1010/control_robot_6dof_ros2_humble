4ce362b94437795699c558bf9392b6f30e2b491c
NguyenVu1010 <vunguyen101003@example.com>
2026-01-20 17:15:14 +0700

    init

--- build/my_arm_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nguyen1/workspace/tu_thanh_17_1/src/robot
-# Build directory: /home/nguyen1/workspace/tu_thanh_17_1/build/my_arm_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_arm_robot/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/nguyen1/workspace/tu_thanh_17_1/install/my_robot_controllers/share/my_robot_controllers/package.sh"
